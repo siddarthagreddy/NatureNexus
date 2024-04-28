6c3dd8b8635ca1b36bd079079ed9dc37d2d57dc7
sgundluru <sgundluru@usf.edu>
2024-04-27 22:52:51 -0400

    Added Startup

--- Program.cs
+++ Program.cs
@@ -1,55 +1,41 @@
-using Microsoft.EntityFrameworkCore;
+using NatureNexus;
 using NatureNexus.Data;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
-namespace NatureNexus
+namespace DISFinalProject
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            var host = CreateHostBuilder(args).Build();
 
-            // Add services to the container.
-            builder.Services.AddControllersWithViews();
-            builder.Services.AddDbContext<NatureNexusContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("NatureNexusContext") ?? throw new InvalidOperationException("Connection string 'NatureNexusContext' not found.")));
-            builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
-            builder.Services.AddControllersWithViews();
-            builder.Services.AddRazorPages()
-                .AddMvcOptions(options =>
-                {
-                    options.MaxModelValidationErrors = 50;
-                    options.ModelBindingMessageProvider.SetValueMustNotBeNullAccessor(
-                        _ => "The field is required.");
-                });
+            CreateDbIfNotExists(host);
 
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
-
-            app.Run();
+            host.Run();
         }
+        private static void CreateDbIfNotExists(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<NatureNexusContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred creating the DB.");
+                }
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
