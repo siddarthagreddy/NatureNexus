6a725dd11b4c5889339d115ae8c2e4f4cea71944
sgundluru <sgundluru@usf.edu>
2024-04-28 20:07:14 -0400

    DB Issue Fix

--- Program.cs
+++ Program.cs
@@ -1,10 +1,24 @@
+using Microsoft.EntityFrameworkCore;
 using NatureNexus;
 using NatureNexus.Data;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Builder;
 
-namespace DISFinalProject
+namespace NatureNexus
 {
     public class Program
     {
+        public IConfiguration Configuration { get; }
+
+        public Program(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public static void Main(string[] args)
         {
             var host = CreateHostBuilder(args).Build();
@@ -13,6 +27,7 @@ namespace DISFinalProject
 
             host.Run();
         }
+
         private static void CreateDbIfNotExists(IHost host)
         {
             using (var scope = host.Services.CreateScope())
@@ -32,10 +47,48 @@ namespace DISFinalProject
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+     Host.CreateDefaultBuilder(args)
+         .ConfigureWebHostDefaults(webBuilder =>
+         {
+             webBuilder.ConfigureServices((hostingContext, services) =>
+             {
+                 var configuration = hostingContext.Configuration;
+                 services.AddRazorPages().AddRazorRuntimeCompilation();
+                 services.AddControllersWithViews();
+                 services.AddDbContext<NatureNexusContext>(options => options.UseSqlServer(configuration.GetConnectionString("NatureNexusContext")));
+                 services.AddRazorPages()
+                     .AddMvcOptions(options =>
+                     {
+                         options.MaxModelValidationErrors = 50;
+                         options.ModelBindingMessageProvider.SetValueMustNotBeNullAccessor(
+                             _ => "The field is required.");
+                     });
+             })
+             .Configure(app =>
+             {
+                 var env = app.ApplicationServices.GetRequiredService<IWebHostEnvironment>();
+                 if (env.IsDevelopment())
+                 {
+                     app.UseDeveloperExceptionPage();
+                 }
+                 else
+                 {
+                     app.UseExceptionHandler("/Home/Error");
+                 }
+                 app.UseStaticFiles();
+
+                 app.UseRouting();
+
+                 app.UseAuthorization();
+
+                 app.UseEndpoints(endpoints =>
+                 {
+                     endpoints.MapControllerRoute(
+                         name: "default",
+                         pattern: "{controller=Home}/{action=Index}/{id?}");
+                 });
+             });
+         });
+
     }
 }
